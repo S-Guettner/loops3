f94e9ab4bab4f5633cc020738658a2a5645dd2dd
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-06 14:36:39 +0100

    c-p

--- 3_2/main.js
+++ 3_2/main.js
@@ -1,12 +1,14 @@
 let numArr = [5, 22, 15, 100, 55]
 
 for (let i = 0; i < numArr.length; i++) {
+	
 	for (let n = 2; n < numArr[i]; n++) {
-		if (numArr[i] % n == 0) {
-			let x = numArr[i] / n;
+	
+		if (numArr[i] % n === 0) {
+			let output  = numArr[i] / n
 			document.write(
-				`${numArr[i]} lässt sich durch ${n} teilen. Das Ergebnis ist ${x}.<br>`
-			);
+				`${numArr[i]} lässt sich durch ${n} teilen. Das Ergebnis ist ${output}.<br>`
+			)
 		}
 	}
 }
